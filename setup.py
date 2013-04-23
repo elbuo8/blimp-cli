0d7d303181162bad12632b952f17e46840013521
elbuo8 <yamil.asusta@upr.edu>
2013-04-22 20:47:23 -0400

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='blimp-cli',
+    version='0.2',
+    author='Yamil Asusta',
+    author_email='yamil.asusta@upr.edu',
+    scripts=['blimp'],
+    url='https://github.com/elbuo8/blimp-cli/',
+    download_url='https://github.com/elbuo8/blimp-cli/tarball/master',
+    license='MIT License',
+    description='CLI interface for Blimp Tasks API',
+    long_description='CLI interface for Blimp Tasks API',
+    install_requires=[
+        'blimp'
+    ],
+)
